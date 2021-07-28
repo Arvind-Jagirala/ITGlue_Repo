c5ef2e9ada110c54f535fe0ffb9963311f3b1417
root <root@ip-10-10-0-6.ca-central-1.compute.internal>
2021-07-28 09:27:15 +0000

    added docker seyup file

---
+++ dockersetup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+dnf update -y
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf list docker-ce
+dnf install docker-ce --nobest -y
+systemctl start docker
+systemctl enable docker
+docker --version
+dnf install curl -y
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+yum install -y git
+sleep 30
+git clone https://github.com/Arvind-Jagirala/ITGlue_Repo.git 
+cd ITGlue_Repo
+docker-compose up --build
+sleep 90
+
